779b91e457660f0c2dcb57459f1a08e354bc548f
Gabriel Godoy <gabyreload@gmail.com>
2021-09-27 16:32:16 -0300

    require cookie-parser

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // middleware
 app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
-app.use(express.json());
 
 // database connection
 const dbURI =
