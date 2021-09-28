53433885913840f57827834f22f302b6e4a8cdfd
Gabriel Godoy <gabyreload@gmail.com>
2021-09-28 07:33:30 -0300

    if the enviroment variables its not defined, the program exit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
 const { requireAuth, checkUser } = require("./middleware/auth");
+const config = require("config");
 
 const app = express();
 
@@ -15,8 +16,14 @@ app.use(cookieParser());
 app.set("view engine", "ejs");
 
 // database connection
-const dbURI =
-  "mongodb+srv://reload:Jano1987@cluster0.4hbz9.mongodb.net/node-auth";
+
+if (!config.get("db")) {
+  console.log("FATAL ERROR: db is not defined...");
+  process.exit(1);
+}
+
+const db = config.get("db");
+const dbURI = `mongodb+srv://reload:${db}@cluster0.4hbz9.mongodb.net/node-auth`;
 mongoose
   .connect(dbURI, {
     useNewUrlParser: true,
@@ -25,7 +32,14 @@ mongoose
   })
   .then((result) => {
     console.log("Connected to mongoDB...");
-    app.listen(3000);
+
+    if (!config.get("jwtPrivateKey")) {
+      console.log("FATAL ERROR: jwtPrivateKey is not defined...");
+      process.exit(1);
+    } else {
+      const port = process.env.PORT || 3000;
+      app.listen(port, () => console.log(`Listening on port ${port}...`));
+    }
   })
   .catch((err) => console.log(err));
 
